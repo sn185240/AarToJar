b963c2db46704d345faae382625bd369bad17188
Shashank Madhav <sn185240@ncr.com>
2024-02-21 12:13:23 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import com.ncr.silverpro.restaurant.wrapper.AppliedDiscount;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static {
-    System.loadLibrary("poswrapper");
+    System.loadLibrary("libposwrapper");
   }
 
     public static void main(String[] args) {
